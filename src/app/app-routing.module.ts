1f34b34cefb89a6f093875ab9544ed2b6917976f
casianaoprut <oprunicoletacasiana@yahoo.com>
2021-10-20 16:20:03 +0300

    Added a pet model and some routing paths.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home-page', pathMatch: 'full'},
+  {path: 'home-page', component: HomePageComponent},
+  {path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/model/pet.model.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat";
+import Timestamp = firebase.firestore.Timestamp;
+
+export interface Pet{
+  name: string;
+  breed: string;
+  description?: string;
+  photoURL: string;
+  addedDate: Timestamp;
+  birthDate: Timestamp;
+  gender: string;
+  id?: string;
+  photoPath?: string;
+  adopted: boolean;
+}
