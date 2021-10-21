e1af493d7b2cbbe93741018e6be257ed34716df9
casianaoprut <oprunicoletacasiana@yahoo.com>
2021-10-21 13:52:54 +0300

    Created the admin guard.

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -6,6 +6,7 @@ import {FormsModule} from "@angular/forms";
 import {PrimengModule} from "../primeng.module";
 
 import {AuthGuard} from "../shared/guards/auth.guard";
+import {AdminGuard} from "../shared/guards/admin.guard";
 
 import { SaleFormComponent } from './sale-form/sale-form.component';
 import { SaleFormsManagerComponent } from './sale-forms-manager/sale-forms-manager.component';
@@ -23,9 +24,9 @@ import { AcceptedFormListComponent } from './accepted-form-list/accepted-form-li
     CommonModule,
     RouterModule.forChild([
       {path: '', component: SaleFormComponent, canActivate: [AuthGuard]},
-      {path: 'manager', component: SaleFormsManagerComponent, canActivate: [AuthGuard]},
+      {path: 'manager', component: SaleFormsManagerComponent, canActivate: [AdminGuard]},
       {path: 'my-forms', component: UserFormListComponent, canActivate: [AuthGuard]},
-      {path: 'accepted-forms', component: AcceptedFormListComponent, canActivate: [AuthGuard]}
+      {path: 'accepted-forms', component: AcceptedFormListComponent, canActivate: [AdminGuard]}
     ]),
     FormsModule,
     PrimengModule
---
+++ src/app/shared/guards/admin.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+
+import { Observable } from 'rxjs';
+import {map, take, tap} from "rxjs/operators";
+
+import {AuthService} from "../../auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.user$.pipe(
+      take(1),
+      map(user => {
+        if (user && user.admin === true ){
+          return true;
+        } else {
+          return false;
+        }
+      }),
+      tap(loggedIn => {
+        if (!loggedIn){
+          this.router.navigate(['/home-page']);
+        }
+      })
+    );
+
+  }
+
+}
